7f39ea0ba94463744524631b3a3f168640c94d08
Varma Nallapuraju <70928292+varma1131987@users.noreply.github.com>
2025-11-25 08:52:20 +0530

    day1 practice

---
+++ Day_1/2_Helloworld.py
@@ -0,0 +1,7 @@
+print("hello world")
+
+# Hw : Unit Test case
+def test_hello():   
+    assert "hello world" == "hello world"
+
+##command : pytest -v Day_1/2_Helloworld.py
---
+++ Day_1/test_main.py
@@ -0,0 +1,20 @@
+"""Import unittest and unittest.mock: These are used for testing and mocking the print function.
+Patch the print function: The @patch decorator replaces print with a mock object to capture its calls.
+Assert the output: Verify that print was called with the expected string.
+You can run the tests using the following command in the terminal"""
+
+import unittest
+from unittest.mock import patch
+from main import main
+
+class TestMainFunction(unittest.TestCase):
+    @patch("builtins.print")
+    def test_main_output(self, mock_print):
+        # Call the main function
+        main()
+        # Assert that print was called with the expected output
+        mock_print.assert_called_once_with("Hello from python-learnings!")
+
+if __name__ == "__main__":
+    unittest.main()
+
