2bbac364f22924dc28da3951c6e9cce6be24f311
Varma Nallapuraju <70928292+varma1131987@users.noreply.github.com>
2025-11-26 07:38:51 +0530

    basics programs

--- Day_1/test_main.py
+++ Day_1/test_main.py
@@ -18,3 +18,5 @@ class TestMainFunction(unittest.TestCase):
 if __name__ == "__main__":
     unittest.main()
 
+    
+
---
+++ Day_2/turtel_basics.py
@@ -0,0 +1,12 @@
+import turtle
+
+student_turtle = turtle.Turtle()
+
+student_turtle.forward(100)
+student_turtle.right(90)
+student_turtle.forward(200)
+student_turtle.right(90)
+student_turtle.forward(100)
+student_turtle.right(90)
+student_turtle.forward(200)
+
